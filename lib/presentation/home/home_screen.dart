899e69a07170d17f0615c013037540f90ddb1464
Ammar-IT99 <elkingamar44@gmail.com>
2025-02-09 04:54:11 +0200

    add files

---
+++ lib/presentation/home/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const String routeName = 'Home Screen';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+
+    );
+  }
+}
